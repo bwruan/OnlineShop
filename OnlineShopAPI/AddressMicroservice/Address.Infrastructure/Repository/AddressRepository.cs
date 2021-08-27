99f4f7553a364c0caac2e1472d1e028ac67334de
Becky <becky.ruan524@gmail.com>
2021-08-26 20:44:16 -0700

    entered some address repository methods

--- OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Repository/AddressRepository.cs
+++ OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Repository/AddressRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Address.Infrastructure.Repository.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +10,37 @@ namespace Address.Infrastructure.Repository
 {
     public class AddressRepository
     {
-        public async Task AddAddress()
+        public async Task AddAddress(string shippingAdd, long accountId)
         {
+            using(var context = new OnlineShopContext())
+            {
+                var address = new Entities.Address()
+                {
+                    Shipping = shippingAdd,
+                    AccountId = accountId
+                };
 
+                context.Addresses.Add(address);
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<List<Entities.Address>> GetAddressesByAccountId(long accountId)
+        {
+            using (var context = new OnlineShopContext())
+            {
+                var addresses = await context.Addresses.Where(a => a.AccountId == accountId).ToListAsync();
+
+                var addressList = new List<Entities.Address>();
+
+                foreach (var address in addresses)
+                {
+                    addressList.Add(address);
+                }
+
+                return addressList;
+            }
         }
     }
 }
