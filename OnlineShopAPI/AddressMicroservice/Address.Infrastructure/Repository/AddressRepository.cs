976e1ce8b1bb67c5535c2484e5782d7da17f5164
Becky <becky.ruan524@gmail.com>
2021-08-26 20:20:35 -0700

    Added domain, api and unit test projects

--- OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Repository/AddressRepository.cs
+++ OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Repository/AddressRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Address.Infrastructure.Repository
 {
     public class AddressRepository
     {
+        public async Task AddAddress()
+        {
+
+        }
     }
 }
