9463850194179bc4f074b79c7578b6b324a8ed59
Becky <becky.ruan524@gmail.com>
2021-08-26 19:18:20 -0700

    EF entities and repository class made

--- OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Address.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ OnlineShopAPI/AddressMicroservice/Address.Infrastructure/Repository/AddressRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address.Infrastructure.Repository
+{
+    public class AddressRepository
+    {
+    }
+}
