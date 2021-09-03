bb002dbed509727102fcff333f7bfd741b67ceef
Becky <becky.ruan524@gmail.com>
2021-09-02 19:43:32 -0700

    address controller

---
+++ OnlineShopAPI/AddressMicroservice/Address.Api/Models/AddAddressRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Address.Api.Models
+{
+    public class AddAddressRequest
+    {
+        public long AddressId { get; set; }
+
+        public string Shipping { get; set; }
+
+        public long AccountId { get; set; }
+    }
+}
---
+++ OnlineShopAPI/AddressMicroservice/Address.Api/Models/UpdateAddressRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Address.Api.Models
+{
+    public class UpdateAddressRequest
+    {
+        public long AddressId { get; set; }
+
+        public string NewShipping { get; set; }
+    }
+}
