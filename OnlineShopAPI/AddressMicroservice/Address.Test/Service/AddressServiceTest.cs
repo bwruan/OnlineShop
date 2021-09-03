75c4b19696266f9fa8afa5006d10665b7071a709
Becky <becky.ruan524@gmail.com>
2021-09-02 20:14:32 -0700

    controller unit test

---
+++ OnlineShopAPI/AddressMicroservice/Address.Test/Service/AddressServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Address.Test.Service
+{
+    [TestFixture]
+    public class AddressServiceTest
+    {
+    }
+}
