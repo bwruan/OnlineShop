e587232bcc53d6cc69d37fb53dce38fb0d5032ee
Becky <becky.ruan524@gmail.com>
2021-09-11 22:21:13 -0700

    added address environment and address service

---
+++ OnlineShopUI/OnlineShop/src/app/service/address-service.spec.ts
@@ -0,0 +1,7 @@
+import { AddressService } from './address-service';
+
+describe('AddressService', () => {
+  it('should create an instance', () => {
+    expect(new AddressService()).toBeTruthy();
+  });
+});
