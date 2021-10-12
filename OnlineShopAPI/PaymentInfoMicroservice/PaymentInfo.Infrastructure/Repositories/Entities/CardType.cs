563948225dfecfab6d879f1d8398712e4cd87828
Becky <becky.ruan524@gmail.com>
2021-10-11 19:47:48 -0700

    Added Nuget Packages and repository entities

---
+++ OnlineShopAPI/PaymentInfoMicroservice/PaymentInfo.Infrastructure/Repositories/Entities/CardType.cs
@@ -0,0 +1,9 @@
+﻿namespace PaymentInfo.Infrastructure.Repositories.Entities
+{
+    public class CardType
+    {
+        public long CardTypeId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ OnlineShopAPI/PaymentInfoMicroservice/PaymentInfo.Infrastructure/Repositories/Entities/Payment.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaymentInfo.Infrastructure.Repositories.Entities
+{
+    class Payment
+    {
+    }
+}
