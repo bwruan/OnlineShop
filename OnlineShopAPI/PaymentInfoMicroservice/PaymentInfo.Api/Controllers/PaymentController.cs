e4b6e79c286ffc6e666ccd52d37e96a343a91c85
Becky <becky.ruan524@gmail.com>
2021-10-17 18:42:55 -0700

    api controller

--- OnlineShopAPI/PaymentInfoMicroservice/PaymentInfo.Api/Controllers/PaymentController.cs
+++ OnlineShopAPI/PaymentInfoMicroservice/PaymentInfo.Api/Controllers/PaymentController.cs
@@ -35,5 +35,94 @@ namespace PaymentInfo.Api.Controllers
                 return StatusCode(500, ex.Message);
             }
         }
+
+        [HttpGet("getPayment/{paymentId}")]
+        public async Task<IActionResult> GetPaymentByPaymentId(long paymentId)
+        {
+            try
+            {
+                var token = "";
+
+                if (Request.Headers.ContainsKey("Authorization"))
+                {
+                    var jwt = (Request.Headers.FirstOrDefault(s => s.Key.Equals("Authorization"))).Value;
+
+                    if (jwt.Count <= 0)
+                    {
+                        return StatusCode(400);
+                    }
+
+                    token = jwt[0].Replace("Bearer ", "");
+                }
+
+                var payment = await _paymentService.GetPaymentByPaymentId(paymentId, token);
+
+                return Ok(payment);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("paymentList/{accountId}")]
+        public async Task<IActionResult> GetPaymentsByAccountId(long accountId)
+        {
+            try
+            {
+                var token = "";
+
+                if (Request.Headers.ContainsKey("Authorization"))
+                {
+                    var jwt = (Request.Headers.FirstOrDefault(s => s.Key.Equals("Authorization"))).Value;
+
+                    if (jwt.Count <= 0)
+                    {
+                        return StatusCode(400);
+                    }
+
+                    token = jwt[0].Replace("Bearer ", "");
+                }
+
+                var paymentList = await _paymentService.GetPaymentsByAccountId(accountId, token);
+
+                return Ok(paymentList);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPut("updatePayment")]
+        public async Task<IActionResult> UpdatePayment([FromBody] UpdatePaymentRequest request)
+        {
+            try
+            {
+                await _paymentService.UpdatePayment(request.PaymentId, request.NewNameOnCard, request.NewCardNumber, request.NewSecurityCode, request.NewExpDate, request.NewBillingName,
+                    request.NewBillingUnit, request.NewBillingCity, request.NewBillingState, request.NewBillingZipcode, request.NewCardType);
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpDelete("deletePayment/{paymentId}")]
+        public async Task<IActionResult> DeletePayment(long paymentId)
+        {
+            try
+            {
+                await _paymentService.DeletePayment(paymentId);
+
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
